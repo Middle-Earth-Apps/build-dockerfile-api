e8d287a7bf8695d5d4898b170ffa7fa3d79dd5d8
mthenry <mikethenry@proton.me>
2026-01-21 13:39:07 -0600

    update

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\DockerImageBuilderController;
 use App\Http\Controllers\ManifestController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware(['api.auth'])->group(function () {
     Route::post('/argo-app', [ArgoAppManifestController::class, 'index']);
     Route::post('/docker/build', [DockerImageBuilderController::class, 'index']);
